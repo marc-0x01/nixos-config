999dca150742975474048bfdc13f85b857be6098
Marc Guillen <marc@0x01.ooo>
2023-11-27 18:00:14 +0100

    (Stylix theme) Tweaked the Amber colorscheme and changed the font to Terminus

--- home/themes/amber.nix
+++ home/themes/amber.nix
@@ -10,30 +10,50 @@
       url = "https://images7.alphacoders.com/883/883131.jpg";
       sha256 = "d382f82c88cc646003c4c63a3fe08fe5505a14a13d9fdb72b936d5ec4a01a293";
     };
-    # Monochrome and dark
+    # Amber, old school
+    base16Scheme = rec {
+      scheme = "Amber Screen";
+      author = "Marc Guillen (https://github.com/marc-0x01)";
+      base00 = "282828"; # ----
+      base01 = "2e1c00"; # ---
+      base02 = "9e6c00"; # --
+      base03 = "be8200"; # -
+      base04 = "de9900"; # +
+      base05 = "ffb000"; # ++
+      base06 = "ffb940"; # +++
+      base07 = "ffc15d"; # ++++
+      base08 = base03;
+      base09 = base04;
+      base0A = base03;
+      base0B = base05;
+      base0C = base02;
+      base0D = base04;
+      base0E = base05;
+      base0F = base02;
+    };
     polarity = "dark";
     # Opacities
     opacity = {
-       terminal = 0.90;
+       terminal = 0.95;
     };
     # Fonts
     fonts = {
       serif = {
-        name = "BigBlueTermPlus Nerd Font";
+        name = "Terminess Nerd Font";
         package = pkgs.nerdfonts.override {
-          fonts = ["BigBlueTerminal" ];
+          fonts = ["Terminus" ];
         };
       };
       sansSerif = {
-        name = "BigBlueTermPlus Nerd Font";
+        name = "Terminess Nerd Font";
         package = pkgs.nerdfonts.override {
-          fonts = ["BigBlueTerminal" ];
+          fonts = ["Terminus" ];
         };
       };
       monospace = {
-        name = "BigBlueTermPlus Nerd Font";
+        name = "Terminess Nerd Font";
         package = pkgs.nerdfonts.override {
-          fonts = ["BigBlueTerminal" ];
+          fonts = ["Terminus" ];
         };
       };
       emoji = {
@@ -44,7 +64,7 @@
       };
       # Font Sizes by context
       sizes = {
-        terminal = 13;
+        terminal = 15;
       };
     };
   };
