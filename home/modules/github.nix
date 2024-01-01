d464a4e872497e01026071098884ff5872660a16
Marc Guillen <marc@0x01.ooo>
2024-01-01 21:57:40 +0100

    Add basic configuration for gh and the dash extension

--- home/modules/github.nix
+++ home/modules/github.nix
@@ -9,6 +9,46 @@
       # look for OSPO tools gh-net, gh-sbom
       gh-eco
     ];
+    settings = {
+    };
+  };
+
+  # Enable the popular dashboard (gh-dash)
+  # NOTE: It has is own configuration, see https://github.com/dlvhdr/gh-dash
+  programs.gh-dash = {
+    enable = true;
+    settings = { 
+      # Specificy PR/filters
+      prSections = [
+        {
+          title = "PR Created";
+          filters = "is:open author:@me";
+        }
+        {
+          title = "PR to Reviews";
+          filters = "is:open review-requested:@me";
+        }
+        {
+          title = "PR Involved";
+          filters = "is:open involves:@me -author:@me";
+        }
+      ];
+      # Specificy Issues/filters
+      issuesSections = [
+        {
+          title = "Created";
+          filters = "is:open author:@me";
+        }
+        {
+          title = "Assigned";
+          filters = "is:open assignee:@me";
+        }
+        {
+          title =  "Involved";
+          filters = "is:open involves:@me -author:@me"; 
+        }
+      ];
+    };
   };
 
 }
