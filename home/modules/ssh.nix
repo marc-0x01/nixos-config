d142806cf45ba9890181c12d883d9d4a242e5254
Marc Guillen <marc@0x01.ooo>
2024-02-09 17:27:29 +0100

    Added pro config for GitHub

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -21,6 +21,13 @@
         identitiesOnly = true;
         identityFile = "~/.ssh/id-ed25519.key";
       };
+      "github.com-pro" = {
+        host = "github.com"; 
+        compression = true;
+        user = "mguillen";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/id-ed25519-pro.key";
+      };
       "0x01.cloud" = {
         host = "*.0x01.cloud"; 
         compression = true;
