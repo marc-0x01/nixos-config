ffc7fca07cfc9894780c291ebfc538f78072a8c8
marc <marc@0x01.ooo>
2024-08-02 09:39:12 +0200

    Initial basic configuration for sketchybar, to be changed by the theme in homemanager config

--- systems/modules/sketchybar.nix
+++ systems/modules/sketchybar.nix
@@ -8,38 +8,36 @@
     config = ''
         ## General Appearance and Defaults
         # Bar Appearance
-        sketchybar --bar     \ 
-            height=37        \
-            blur_radius=30   \
-            position=top     \
-            sticky=on        \
-            padding_left=10  \
-            padding_right=10 \
-            color=0xff101314
+        sketchybar --bar      \
+            height=38         \
+            position=top      \
+            sticky=off        \
+            y_offset=-1       \
+            padding_left=20   \
+            padding_right=-1  \
+            color=0xff
         # Changing Defaults
-        sketchybar --default \ 
-            icon.font="SF Pro:Semibold:15.0"  \
-            icon.color=WHITE=0xffffffff       \
-            label.font="SF Pro:Semibold:15.0" \
-            label.color=WHITE=0xffffffff      \
-            background.color=0xff353c3f       \
-            background.corner_radius=5        \
-            background.height=24              \
-            padding_left=5                    \
-            padding_right=5                   \
-            label.padding_left=4              \
-            label.padding_right=10            \
-            icon.padding_left=10              \
-            icon.padding_right=4
+        sketchybar --default \
+            icon.font="Helvetica:Regular:15.0"  \
+            icon.padding_left=-1                \
+            icon.padding_right=-1               \
+            label.font="Helvetica:Regular:15.0"
         ## Items
-        # TODO
+        # No Custom Items
         ## Layouts
         # Left Items
-        # TODO
+        sketchybar --add item logo left
+        sketchybar --set logo \
+            label.string="(oO,) \,,/" \
+            label.color=0xff000000    \
+            label.font.style=Bold
         # Notch Items
-        # TODO
+        # Nothing in notch area!
         # Right Items
-        # TODO
+        sketchybar --add alias "Control Center,Clock" right
+        sketchybar --add alias "Control Center,WiFi" right
+        sketchybar --add alias "Control Center,Battery" right
+        sketchybar --add alias "Control Center,Sound" right
         ## Force all scripts to run the first time
         sketchybar --update
     '';
