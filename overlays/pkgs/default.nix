b63c86e05e37e17ce00eeda5f213621b181f14e8
marc <marc@0x01.ooo>
2024-08-06 01:21:37 +0200

    Wrong approach to overlays, deleting file

--- overlays/pkgs/default.nix
+++
@@ -1,9 +0,0 @@
-# Custom packages 
-# They can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example'
-
-{ pkgs }: {
-
-  qutebrowser-bin = pkgs.callPackage ./quutebrowser-bin { };
-
-}
