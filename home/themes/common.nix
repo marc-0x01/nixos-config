8a5b260baa94bcb124cd8cdefc61f505ebb39692
marc <marc@0x01.ooo>
2024-08-08 07:31:19 +0200

    Expanding on other plugins

--- home/themes/common.nix
+++ home/themes/common.nix
@@ -24,6 +24,48 @@
       source = ./scripts/sketchybar-plugins/calendar.sh;
       target = "${config.home.homeDirectory}/.config/sketchybar/plugins/calendar.sh";
     };
+      ".config/sketchybar/plugins/volume.sh" = {
+      enable = pkgs.stdenv.isDarwin;
+      executable = true;
+      source = ./scripts/sketchybar-plugins/volume.sh;
+      target = "${config.home.homeDirectory}/.config/sketchybar/plugins/volume.sh";
+    };
+      ".config/sketchybar/plugins/cpu.sh" = {
+      enable = pkgs.stdenv.isDarwin;
+      executable = true;
+      source = ./scripts/sketchybar-plugins/cpu.sh;
+      target = "${config.home.homeDirectory}/.config/sketchybar/plugins/cpu.sh";
+    };
+      ".config/sketchybar/plugins/battery.sh" = {
+      enable = pkgs.stdenv.isDarwin;
+      executable = true;
+      source = ./scripts/sketchybar-plugins/battery.sh;
+      target = "${config.home.homeDirectory}/.config/sketchybar/plugins/battery.sh";
+    };
+      ".config/sketchybar/plugins/front-app.sh" = {
+      enable = pkgs.stdenv.isDarwin;
+      executable = true;
+      source = ./scripts/sketchybar-plugins/front-app.sh;
+      target = "${config.home.homeDirectory}/.config/sketchybar/plugins/front-app.sh";
+    };
+      ".config/sketchybar/plugins/media.sh" = {
+      enable = pkgs.stdenv.isDarwin;
+      executable = true;
+      source = ./scripts/sketchybar-plugins/media.sh;
+      target = "${config.home.homeDirectory}/.config/sketchybar/plugins/media.sh";
+    };
+      ".config/sketchybar/plugins/space.sh" = {
+      enable = pkgs.stdenv.isDarwin;
+      executable = true;
+      source = ./scripts/sketchybar-plugins/space.sh;
+      target = "${config.home.homeDirectory}/.config/sketchybar/plugins/space.sh";
+    };
+      ".config/sketchybar/plugins/space-windows.sh" = {
+      enable = pkgs.stdenv.isDarwin;
+      executable = true;
+      source = ./scripts/sketchybar-plugins/space-windows.sh;
+      target = "${config.home.homeDirectory}/.config/sketchybar/plugins/space-windows.sh";
+    };
     # ...
   };
 
