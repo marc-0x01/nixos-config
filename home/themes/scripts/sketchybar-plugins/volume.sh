f6ea0d860a46138b5b487f42f44a0766fed61aa4
marc <marc@0x01.ooo>
2024-08-08 16:10:31 +0200

    Fix battery icons

--- home/themes/scripts/sketchybar-plugins/volume.sh
+++ home/themes/scripts/sketchybar-plugins/volume.sh
@@ -4,13 +4,13 @@ if [ "$SENDER" = "volume_change" ]; then
   VOLUME=$INFO
 
   case $VOLUME in
-    [6-9][0-9]|100) ICON=""
+    [6-9][0-9]|100) ICON="󰕾"
     ;;
-    [3-5][0-9]) ICON=""
+    [3-5][0-9]) ICON="󰖀"
     ;;
-    [1-9]|[1-2][0-9]) ICON=""
+    [1-9]|[1-2][0-9]) ICON="󰕿"
     ;;
-    *) ICON=""
+    *) ICON="󰖁"
   esac
 
   sketchybar --set volume icon="$ICON" label="$VOLUME%"
