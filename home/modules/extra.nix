1958f1eb9c009feb85adf889058a9534aca80a69
marc <marc@0x01.ooo>
2024-09-04 18:36:56 +0200

    Cleanup of extra programs

--- home/modules/extra.nix
+++ home/modules/extra.nix
@@ -2,29 +2,28 @@
 
 { pkgs, lib, config, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
 
-  # Extra packages or not yet in home-manager
-  # Look for new addition or rust alternatives: lolcrab (better lolcat)
+  # Extra packages not yet in home-manager
+  # Look for new addition or rust alternatives: 
+  # lolcrab, charasay
+
   home.packages = with pkgs; [
 
     # Desktop Apps
     obsidian                  # Digital Garden and noote taking
     jetbrains.gateway         # Remote development, alternative to vim in a professional environment
 
-    # Terminal
-    charasay                  # Cow have voice in terminal, better coesay (charasay)
-    lolcat                    # Rainbow! (lolcat)
-    steampipe                 # Query like it's 1992 (steampipe)
-    uutils-coreutils          # Better coreutils in rust, hamonize on darwin (*)
+    # Utils
+    uutils-coreutils-noprefix # Better coreutils in rust, harmonize on darwin (*)
     ouch                      # Compression swiss-army knife (ouch)
-    rsign2                    # Signing cli compatible with minisign (rsign)
     rage                      # A simple, secure and modern encryption tool (age)
-    age-plugin-yubikey        # -- Integration of age with yubikey
-    difftastic                # Better diff (difft), mostly for git
-    macchina                  # System info fetcher (macchina)
     xh                        # Replacement for httpie, curl (xh)
     du-dust                   # More intuitive du (dust)
-    vulnix                    # Nix scurity scanner
+    macchina                  # System info fetcher (macchina)
+    rsign2                    # Signing cli compatible with minisign (rsign)
 
+    # Terminal
+    steampipe                 # Query like it's 1992 (steampipe)
+    
   ];
 
 }
