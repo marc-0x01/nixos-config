dc7a69b034bf86153352f7d1aa0b9034d66bbcff
marc <marc@0x01.ooo>
2024-09-04 22:01:49 +0200

    Base vim config: nvim+mini.vim plugins

--- home/modules/vim.nix
+++ home/modules/vim.nix
@@ -2,8 +2,34 @@
 
 { pkgs, lib, config, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
 
+  # Still there...
   programs.vim = {
     enable = true;
+  };
+  
+  # ... But we will use nvim as a replacement
+  # - Using the the mini.vim plugins for a sensible configuration
+  programs.neovim = {
+    enable = true;
+    # Alias
+    viAlias = true;
+    vimAlias = true;
+    # Plugins
+    plugins = with pkgs.vimPlugins; [
+      # Using mini.vim for a base editor
+      mini-nvim
+    ];
+    # Config
+    extraLuaConfig = ''
+      require('mini.starter').setup()
+      require('mini.basics').setup()
+      require('mini.statusline').setup()
+      require('mini.tabline').setup()
+      require('mini.animate').setup()
+      require('mini.bufremove').setup()
+      require('mini.cursorword').setup()
+      require('mini.indentscope').setup()
+    '';
   };
 
 }
