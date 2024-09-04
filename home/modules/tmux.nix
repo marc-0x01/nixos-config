f940c83272ec829ead852b0665434c916a4972d6
marc <marc@0x01.ooo>
2024-09-05 00:10:19 +0200

    Base tmux configuration

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -4,6 +4,20 @@
 
   programs.tmux = {
     enable = true;
+    # Session Manager
+    tmuxp.enable = true;
+    # Plugins
+    plugins = with pkgs; [
+      # Using official plugins
+      tmuxPlugins.sensible
+      tmuxPlugins.tmux-window-name
+      tmuxPlugins.sessionist
+      tmuxPlugins.pain-control
+      tmuxPlugins.prefix-highlight
+      tmuxPlugins.yank
+    ];
+    # Config
+    extraConfig = ''''; 
   };
 
 }
